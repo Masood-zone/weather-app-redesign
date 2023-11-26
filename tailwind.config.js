fad554cdec8418f7392abfc8b086bc639b73bf74
Masood <masoodacheampong@gmail.com>
2023-11-26 23:16:42 +0000

    Repurposed weather app

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1.2rem",
+      screens: {
+        xl: "1200px",
+        "2xl": "1200px",
+        md: "650px",
+        sm: "375px",
+      },
+    },
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
